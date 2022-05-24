98b34067dcc5f14bdf527d05bdc38a2549cda6a3
Rohit Rao <rohit@rohitsrao.com>
2022-05-24 21:27:08 +0200

    Added errorHandling for duplicate user signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
@@ -13,15 +14,25 @@ export class AuthService {
   }
 
   async signup(dto: AuthDto) {
-    const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash: hash
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash
+          }
+      });
+      delete user.hash;
+      return user;
+    }
+    catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken')
         }
-    });
-    delete user.hash;
-    return user;
+      }
+      throw err;
+    }
   }
 
 }
