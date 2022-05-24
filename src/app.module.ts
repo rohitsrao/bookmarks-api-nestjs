135df67e6c63f0eb860ed1baebce44bb6a0d8404
Rohit Rao <rohit@rohitsrao.com>
2022-05-24 19:59:45 +0200

    Created PrismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarksModule],
+  imports: [AuthModule, UserModule, BookmarksModule, PrismaModule],
   controllers: [],
   providers: [],
 })
