d9bd57ca727a9156daae1aa53f31b9655b58b419
Rohit Rao <rohit@rohitsrao.com>
2022-05-24 15:55:51 +0200

    Added String return statements in signIn and signUp AuthController methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('sigin')
+  signIn() {
+    return 'I am signed in';
+  }
+
+  @Post('signup')
+  signUp () {
+    return 'I am signed up';
+  }
 }
