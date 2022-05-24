aad4315d741b51799bae2127bf4ebb9bcaa88b4c
Rohit Rao <rohit@rohitsrao.com>
2022-05-24 20:58:55 +0200

    Passed AuthDto instance from controller to service. Installed Argon2 for hashing

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
-  }))
+  }));
   await app.listen(3333);
 }
 bootstrap();
