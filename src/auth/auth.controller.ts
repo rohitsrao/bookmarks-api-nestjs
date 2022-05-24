eaa2f5243163e5a2d401ee5cc88c3557d4c88dbb
Rohit Rao <rohit@rohitsrao.com>
2022-05-24 20:46:38 +0200

    Created dto for Auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post} from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto/auth.dto";
 
 @Controller('auth')
 export class AuthController {
@@ -11,7 +12,8 @@ export class AuthController {
   }
 
   @Post('signup')
-  signUp () {
+  signUp (@Body() dto: AuthDto) {
+    console.log({dto});
     this.authService.signup();
   }
 }
