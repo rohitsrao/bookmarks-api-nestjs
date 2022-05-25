32a8140cca1550c27cc285c75b71b3a4b1bf9a67
Rohit Rao <rohit@rohitsrao.com>
2022-05-25 19:53:47 +0200

    Added a config module so that environment variables do not have to be hardcoded

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarksModule, PrismaModule],
+  imports: [
+    AuthModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule, 
+    BookmarksModule, 
+    PrismaModule
+  ],
   controllers: [],
   providers: [],
 })
